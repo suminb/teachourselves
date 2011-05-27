bbe9dc2a1fafe8b1c35b4acf84d535fb633ec00d
Sumin Byeon <suminb@gmail.com>
2011-05-26 18:06:16 -0700

    I'm going home.

---
+++ to.js
@@ -0,0 +1,30 @@
+
+
+window.onload = function() {
+    // The following code was copied from
+    // http://stackoverflow.com/questions/2161906/handle-url-anchor-change-event-in-js
+    if ("onhashchange" in window) { // event supported?
+        window.onhashchange = function () {
+            hashChanged(window.location.hash);
+        }
+    }
+    else { // event not supported:
+        var storedHash = window.location.hash;
+        window.setInterval(function () {
+            if (window.location.hash != storedHash) {
+                storedHash = window.location.hash;
+                hashChanged(storedHash);
+            }
+        }, 100);
+    }
+};
+
+function hashChanged(hash) {
+    if(hash.match(/^#page-\d+$/)) {
+        setPage(hash);
+    }
+}
+
+function setPage(page) {
+    console.log("set page to " + page);
+}
